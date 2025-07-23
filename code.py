231c5f91d9665f64cbb5fe4634905bc482d63efb
AnvitaDey <anvita.dey.3340@gmail.com>
2025-07-23 18:07:53 +0530

    basic operators

---
+++ code.py
@@ -0,0 +1,13 @@
+operators= input("enter an operator (+,-,*,/):")
+N1= float(input("enter first number :"))
+N2= float(input("enter second number :"))
+if operators== "+":
+    print(N1+N2)
+elif operators== "-":
+    print(N1-N2)
+elif operators== "*":
+    print(N1*N2)
+elif operators== "/":
+    print(N1/N2)
+else:
+    print("invalid input")
